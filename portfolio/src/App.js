98f90eda0fcc1de645e4d697723aeaa2e428162f
thesameakpalu <eakpalu008@st.ug.edu.gh>
2024-05-27 17:52:59 +0000

    set up my environment for the project

---
+++ portfolio/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <p className='text-red-600'>PORTFOLIO WEBSITE</p>
+    </div>
+  );
+}
+
+export default App;
